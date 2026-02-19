e68122aeaed7c8cc4e7afae9ac949caee7df4ea6
huynh hoang <huynhhoang712003@gmail.com>
2026-02-20 00:57:40 +0700

    Tích hợp Prisma Service vào Shared Module.

--- src/routes/posts/posts.controller.ts
+++ src/routes/posts/posts.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { PostsService } from 'src/posts/posts.service';
+import { PostsService } from 'src/routes/posts/posts.service';
 
 @Controller('posts')
 export class PostsController {
---
+++ src/routes/posts/posts.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/shared/services/prisma.service';
+
+@Injectable()
+export class PostsService {
+  constructor(private readonly prismaService: PrismaService) { }
+  getPosts() {
+    return this.prismaService.post.findMany()
+  }
+  createPost(body: any) {
+    return this.prismaService.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: body.authorId
+      }
+    })
+  }
+}
