4b68eb3f4595b870822953347792977d729a16d5
huynh hoang <huynhhoang712003@gmail.com>
2026-02-20 15:51:56 +0700

    Validate file .env.

--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
+import envConfig from 'src/shared/config';
 import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prismaService: PrismaService) { }
   getPosts() {
+    console.log(envConfig.ACCESS_TOKEN_SECRET);
     return this.prismaService.post.findMany()
   }
   createPost(body: any) {
---
+++ src/shared/config.ts
@@ -0,0 +1,39 @@
+import { plainToInstance } from 'class-transformer';
+import { IsString, validateSync } from 'class-validator';
+import fs from 'fs';
+import 'dotenv/config';
+import path from 'path';
+
+//kiểm tra coi thử có file .env hay chưa
+if (!fs.existsSync(path.resolve('.env'))) {
+  console.log("không tìm thấy file .env");
+  process.exit(1);
+}
+
+class ConfigSchema {
+  @IsString()
+  DATABASE_URL!: string;
+  @IsString()
+  ACCESS_TOKEN_SECRET!: string;
+  @IsString()
+  REFRESH_TOKEN_SECRET!: string;
+  @IsString()
+  ACCESS_TOKEN_EXPIRES_IN!: string;
+  @IsString()
+  REFRESH_TOKEN_EXPIRES_IN!: string;
+}
+const configServer = plainToInstance(ConfigSchema, process.env)
+const e = validateSync(configServer)
+if (e.length > 0) {
+  console.log('các giá trị khai báo không hợp lệ');
+  const errors = e.map((err) => {
+    return {
+      property: err.property,
+      constraints: err.constraints,
+      value: err.value
+    }
+  })
+  throw errors
+}
+const envConfig = configServer;
+export default envConfig;
